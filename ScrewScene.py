c0938204f1270ffb2c56bc48d5b93fcec8048567
noceive <nocivescience@gmail.com>
2022-12-06 02:55:53 -0300

    ahora funciona

--- ScrewScene.py
+++ ScrewScene.py
@@ -2,20 +2,12 @@ from manim import *
 class Screw(VMobject):
     def __init__(self,**kwargs):
         super().__init__(
-            self,
+            
             color=YELLOW_A,
             **kwargs
         )
-        points=[]
-        for i in range(-4,5):
-            if i%2==0:
-                second_coord=-4
-            else:
-                second_coord=4
-            point=np.array([i,second_coord,0])
-            points.append(point)
-        self.set_points_as_corner([
-            points
+        self.set_points_as_corners([
+            RIGHT,LEFT
         ])
 class ScrewScene(Scene):
     def construct(self):
